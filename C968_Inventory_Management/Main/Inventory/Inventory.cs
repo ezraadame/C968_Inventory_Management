bdd2fd0f61eb0e82d5492df6d2bdb13eb566a9d1
ezra <oscaroadame@gmail.com>
2025-06-10 20:42:35 -0500

    Added base methods to Inventory class.

--- C968_Inventory_Management/Main/Inventory/Inventory.cs
+++ C968_Inventory_Management/Main/Inventory/Inventory.cs
@@ -10,31 +10,49 @@ namespace C968_Inventory_Management.Main.Inventory
 {
     public class Inventory
     {
-    //    public List<Product> Products = new List<Product>();
+        public List<Product> Products = new List<Product>();
+        public List<Part> AllParts = new List<Part>();
 
-    //    public void AddProduct(Product product) => Products.Add(Products.FirstOrDefault());
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
 
-    //    public bool RemoveProduct() => true;
+        public bool RemoveProduct(int productID)
+        {
+            return true;
+        }
 
-    //    public Product LookupProduct(int id) => Products.FirstOrDefault();
+        public Products LookupProduct(int productID)
+        {
+            return Products.FirstOrDefault();
+        }
 
-    //    public void UpdateProduct(int ProductID, Product product) => Products.Remove(product);
+        public void UpdateProduct(int productID, Product product)
+        {
+            Console.WriteLine("HI");
+        }
 
+        ///
 
-    //}
+        public void AddPart(Part part)
+        {
+            AllParts.Add(part);
+        }
 
-    //public class InventoryParts : Part
-    //{
-    //    public List<Part> Parts = new List<Part>();
+        public bool DeletePart(Part part)
+        {
+            return true;
+        }
 
-    //    public void AddPart(Part part) => Parts.Add(part);
+        public Part LookupPart(int partID)
+        {
+            return AllParts.FirstOrDefault();
+        }
 
-    //    public bool DeletePart() => true;
-
-    //    public Part LookupPart(int PartID) => Parts?.FirstOrDefault();
-
-    //    public void UpdatePart(int PartID, Part part) => Parts.Remove(part);
-
-
+        public void UpdateParts(int partID, Part part)
+        {
+            Console.WriteLine("Temporary");
+        }
     }
 }
